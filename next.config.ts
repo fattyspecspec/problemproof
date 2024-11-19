26ec7ba57419bb97b71148290489e37c96b5f9ab
fattyspecspec <matt@gigco.com>
2024-11-19 09:42:56 +0000

    Remove deprecated reactRoot option from next.config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    reactRoot: true, // Enables React 18 debugging
-  },
 };
 
 export default nextConfig;
-
